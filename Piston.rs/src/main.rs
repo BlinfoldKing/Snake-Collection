9f29fdf632a2bcc2692d1991203fb9c8f2ce92b7
blinfoldking <ganeshadanu1@gmail.com>
2018-05-27 12:34:27 +0700

    initial snake setup

--- Piston.rs/src/main.rs
+++ Piston.rs/src/main.rs
@@ -10,8 +10,13 @@ use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
 
+enum Direction {
+    UP, DOWN, LEFT, RIGHT
+}
+
 struct Game {
     gl: GlGraphics,
+    snake: Snake,
 }
 
 impl Game {
@@ -22,6 +27,33 @@ impl Game {
 
         self.gl.draw(arg.viewport(), |_c, gl| {
             graphics::clear(Bg, gl)
+        });
+
+        self.snake.render(&mut self.gl, arg);
+    }
+}
+
+struct Snake {
+    posX: i32,
+    posY: i32,
+    dir: Direction    
+}
+
+impl Snake {
+    fn render (&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        use graphics;
+
+        let COLOR: [f32; 4] = [0.9, 0.9, 0.9, 1.0];
+        let square = graphics::rectangle::square(
+            (self.posX * 10) as f64, 
+            (self.posY * 10) as f64,
+            10_f64
+            );
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(COLOR, square, transform, gl);
         })
     }
 }
@@ -34,14 +66,19 @@ fn main() {
 
     let mut window: Window = WindowSettings::new(
         "Snake Game",
-        [500, 500]
+        [500, 500],
     ).opengl(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
     let mut game = Game {
-        gl: GlGraphics::new(opengl)
+        gl: GlGraphics::new(opengl),
+        snake: Snake {
+            posX: 12,
+            posY: 10,
+            dir: Direction::RIGHT        
+        }
     };
 
     let mut events = Events::new(EventSettings::new());
